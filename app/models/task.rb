df6ff9546362061fcd117e2b40a8e7dcf5cea97d
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2022-08-09 23:22:03 +0000

    Generate task resource

---
+++ app/models/task.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  status     :string           default("not_yet_started")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+class Task < ApplicationRecord
+end
